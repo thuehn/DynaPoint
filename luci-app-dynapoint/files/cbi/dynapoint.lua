f1e978923561b6e801bf50b6eb77ee179fa0e84f
Asco <menosmalo753@gmail.com>
2016-08-14 12:33:46 +0200

    added i18n support in luci

--- luci-app-dynapoint/files/cbi/dynapoint.lua
+++ luci-app-dynapoint/files/cbi/dynapoint.lua
@@ -10,51 +10,51 @@ for k, v in pairs(wireless) do
   end
 end
 
-m = Map("dynapoint", "DynaPoint", "Dynamic Access Point Validator and Creator")
+m = Map("dynapoint", "DynaPoint", translate("Dynamic Access Point Validator and Creator"))
 m:chain("wireless")
 
 m1 = Map("wireless") 
 
-aps = m1:section(TypedSection, "wifi-iface", "Access Points")
+aps = m1:section(TypedSection, "wifi-iface", translate("Access Points"))
 aps.addremove = false
 aps.anonymous = true
 aps.template  = "cbi/tblsection"
 
-ssid = aps:option(DummyValue, "ssid", "SSID")
+ssid = aps:option(DummyValue, "ssid", translate("SSID"))
 
-action = aps:option(ListValue, "dynapoint_rule", "action")
+action = aps:option(ListValue, "dynapoint_rule", translate("action"))
 action.widget="select"
-action:value("internet","online")
-action:value("!internet","offline")
-action:value("","not used by dynapoint")
+action:value("internet",translate("online"))
+action:value("!internet",translate("offline"))
+action:value("",translate("not used by dynapoint"))
 action.default = ""
 
-s = m:section(NamedSection, "internet", "rule", "Internet", "Internet connectivity")
+s = m:section(NamedSection, "internet", "rule", translate("Internet"), translate("Internet connectivity"))
 
-hosts = s:option(DynamicList, "hosts", "Target host addresses", "Addresses for checking the availability")
+hosts = s:option(DynamicList, "hosts", translate("Target host addresses"), translate("Addresses for checking the availability"))
 hosts.datatype = "string"
 
-interval = s:option(Value, "interval", "Interval", "How often to check Internet connection in seconds")
+interval = s:option(Value, "interval", translate("Interval"), translate("How often to check Internet connection in seconds"))
 interval.datatype = "uinteger"
 interval.default = "30"
 
-timeout = s:option(Value, "timeout", "Timeout", "Timeout in seconds when trying to check Internet availability of host")
+timeout = s:option(Value, "timeout", translate("Timeout"), translate("Timeout in seconds when trying to check Internet availability of host"))
 timeout.datatype = "uinteger"
 timeout.default = "5"
 
-offline_treshold = s:option(Value, "offline_threshold", "Offline threshold", "After how many times of checking, the connection is considered offline")
+offline_treshold = s:option(Value, "offline_threshold", translate("Offline threshold"), translate("After how many times of checking, the connection is considered offline"))
 offline_treshold.datatype = "uinteger"
 offline_treshold.default = "1"
 
-add_hostname_to_ssid = s:option(Flag, "add_hostname_to_ssid", "Append hostname to ssid", "Append the router's hostname to the SSID when connectivity check fails")
+add_hostname_to_ssid = s:option(Flag, "add_hostname_to_ssid", translate("Append hostname to ssid"), translate("Append the router's hostname to the SSID when connectivity check fails"))
 --add_hostname_to_ssid.enabled = "1"
 --add_hostname_to_ssid.disabled = "0"
 add_hostname_to_ssid.rmempty = false
 
-use_curl = s:option(Flag, "use_curl", "Use curl", "Use curl instead of wget for testing the connectivity. ATTENTION: You need to have curl installed before using this.")
+use_curl = s:option(Flag, "use_curl", translate("Use curl"), translate("Use curl instead of wget for testing the connectivity. ATTENTION: You need to have curl installed before using this."))
 use_curl.rmempty = false
 
-curl_interface = s:option(Value, "curl_interface", "Used interface", "Which interface should curl use. (Use ifconfig to find out)")
+curl_interface = s:option(Value, "curl_interface", translate("Used interface"), translate("Which interface should curl use. (Use ifconfig to find out)"))
 curl_interface.datatype = "string"
 curl_interface:depends("use_curl","1")
 curl_interface.placeholder = "eth0"
