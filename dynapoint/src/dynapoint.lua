58b1edb1fbe8d4d05696de2bf541729f9b3cab95
Asco <menosmalo753@gmail.com>
2016-08-23 14:22:58 +0200

    Added support for multi-AP-scenarios

--- dynapoint/src/dynapoint.lua
+++ dynapoint/src/dynapoint.lua
@@ -15,9 +15,9 @@ uci_cursor:revert("wireless")
 
 
 -- get all config sections with the given type
-function getConfType(conf,type)
+function getConfType(conf_file,type)
   local ifce={}
-  uci_cursor:foreach(conf,type,function(s) ifce[s[".index"]]=s end)
+  uci_cursor:foreach(conf_file,type,function(s) ifce[s[".index"]]=s end)
   return ifce
 end
 
@@ -37,41 +37,14 @@ local curl = tonumber(uci_cursor:get("dynapoint", "internet", "use_curl"))
 if (curl == 1) then
   curl_interface = uci_cursor:get("dynapoint", "internet", "curl_interface")
 end
-
-function print_r ( t )
-  local print_r_cache={}
-  local function sub_print_r(t,indent)
-    if (print_r_cache[tostring(t)]) then
-      print(indent.."*"..tostring(t))
-    else
-      print_r_cache[tostring(t)]=true
-      if (type(t)=="table") then
-        for pos,val in pairs(t) do
-          if (type(val)=="table") then
-            print(indent.."["..pos.."] => "..tostring(t).." {")
-            sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
-            print(indent..string.rep(" ",string.len(pos)+6).."}")
-          elseif (type(val)=="string") then
-            print(indent.."["..pos..'] => "'..val..'"')
-          else
-            print(indent.."["..pos.."] => "..tostring(val))
-          end
-        end
-      else
-        print(indent..tostring(t))
-      end
-    end
-  end
-  if (type(t)=="table") then
-    print(tostring(t).." {")
-    sub_print_r(t,"  ")
-    print("}")
-  else
-    sub_print_r(t,"  ")
-  end
-  print()
+if (tonumber(uci_cursor:get("dynapoint", "internet", "add_hostname_to_ssid")) == 1 ) then
+  localhostname = uci_cursor:get("system", "system", "hostname")
 end
 
+local table_names_rule = {}
+local table_names_not_rule = {}
+local ssids_with_hostname = {}
+local ssids_not_rule = {}
 
 function get_dynapoint_sections(t)
   for pos,val in pairs(t) do
@@ -80,31 +53,26 @@ function get_dynapoint_sections(t)
     elseif (type(val)=="string") then
       if (pos == "dynapoint_rule") then
         if (val == "internet") then
-          table_name_1=t[".name"]
+          table_names_rule[#table_names_rule+1] = t[".name"]
         elseif (val == "!internet") then
-          table_name_0=t[".name"]
+          table_names_not_rule[#table_names_not_rule+1] = t[".name"]
+          if (localhostname) then
+            ssids_not_rule[#ssids_not_rule+1] = t[".ssid"]
+            ssids_with_hostname[#ssids_with_hostname+1] = t[".ssid"].."_"..localhostname
+          end
         end
       end
     end
   end
 end
 
-print_r(getConfType("wireless","wifi-iface"))
+
 --print(table.getn(hosts))
 
 get_dynapoint_sections(getConfType("wireless","wifi-iface"))
 
-if (tonumber(uci_cursor:get("dynapoint", "internet", "add_hostname_to_ssid")) == 1 ) then
-  localhostname = uci_cursor:get("system", "system", "hostname")
-  ssid = uci_cursor:get("wireless", table_name_0, "ssid")
-  print(ssid)
-  ssid2 = ssid.."_"..localhostname
-end
-
-
-
 local online = true
-if (tonumber(uci_cursor:get("wireless", table_name_1, "disabled")) == 1) then
+if (tonumber(uci_cursor:get("wireless", table_names_rule[1], "disabled")) == 1) then
   online = false
 end
 
@@ -132,24 +100,34 @@ end
 function change_wireless_config(switch_to_offline)
   if (switch_to_offline == 1) then
     log.syslog("info","Switched to OFFLINE")
-    if (localhostname) then
-      uci_cursor:set("wireless", table_name_0, "ssid", ssid2)
-      log.syslog("info","Bring up new AP "..ssid2)
-    else
-      log.syslog("info","Bring up new AP "..uci_cursor:get("wireless", table_name_0 , "ssid"))
-    end
-    uci_cursor:set("wireless", table_name_0, "disabled", "0")
-    uci_cursor:set("wireless", table_name_1, "disabled", "1")
-  else
-    uci_cursor:set("wireless", table_name_0 , "disabled", "1")
-    uci_cursor:set("wireless", table_name_1 , "disabled", "0")
-    if (localhostname) then
-      uci_cursor:set("wireless", table_name_0, "ssid", ssid)
-    end
-    log.syslog("info","Switched to ONLINE")
-    log.syslog("info","Bring up new AP "..uci_cursor:get("wireless", table_name_1 , "ssid"))
-  end
 
+    for i = 1, #table_names_not_rule do
+      uci_cursor:set("wireless",table_names_not_rule[i], "disabled", "0")
+      if (localhostname) then
+        uci_cursor:set("wireless", table_names_not_rule[i], "ssid", ssids_with_hostname[i])
+        log.syslog("info","Bring up new AP "..ssids_with_hostname[i])
+      else
+        log.syslog("info","Bring up new AP "..ssids_not_rule[i])
+      end
+    end
+
+    for i = 1, #table_names_rule do
+      uci_cursor:set("wireless",table_names_rule[i], "disabled", "1")
+    end
+
+  else
+    log.syslog("info","Switched to ONLINE")
+    for i = 1, #table_names_not_rule do
+      uci_cursor:set("wireless",table_names_not_rule[i], "disabled", "1")
+      if (localhostname) then
+        uci_cursor:set("wireless", table_names_not_rule[i], "ssid", ssids_not_rule[i])
+      end
+    end
+    for i = 1, #table_names_rule do
+      uci_cursor:set("wireless",table_names_rule[i], "disabled", "0")
+      log.syslog("info","Bring up new AP "..uci_cursor:get("wireless", table_names_rule[i], "ssid"))
+    end
+  end
   uci_cursor:save("wireless")
   ubus:call("network", "reload", {})
 end
